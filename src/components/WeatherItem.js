60a89268dab3376ad246d7b4965db3d2693eeec3
Dongseop Kwon <dongseop@gmail.com>
2020-04-20 12:07:09 +0900

    React Bootstrap을 이용하여 기본 UI 완성 - https://react-bootstrap.github.io/ 참고

---
+++ src/components/WeatherItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function WeatherItem({data}) {
+
+  return (
+    <tr key={data.id}>
+      <td>{data.city}</td>
+      <td>{data.temp}</td>
+      <td>{data.pressure}</td>
+      <td>{data.humidity}</td>
+    </tr>
+  )
+}
